8ec565893e78820562724d11d9b9ce4ca10d1e65
dajhub <dajhub63@gmail.com>
2022-07-23 13:21:23 +0100

    readme file updated & folders added

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# fedora
-Minimal install script
+# Fedora
+Minimal install script.  Having installed a minimal Fedora installation you will, in the command line need to first install git:
+~~~
+$ sudo dnf install git
+$ git clone https://github.com/dajhub/fedora
+$ cd fedora
+$ sudo ./install.sh
+~~~
+
+### Additional installations
+Depends on whether these are wanted beyond the initial install script.  Links for installation:
+- Font manager / viewer: https://github.com/FontManager/font-manager
+- Visual Studio Code: https://code.visualstudio.com/docs/setup/linux
+- QEMU/KVM - virtual machine: https://docs.fedoraproject.org/en-US/quick-docs/getting-started-with-virtualization/ 
+  - To use the VM without entering the root password we need to configure KVM to enable it.  See #4 on the attached link: https://discovery.endeavouros.com/applications/how-to-install-virt-manager-complete-edition/2021/09/
+
+### Everblush
+Theme used is based on [Everblush](https://github.com/Everblush)
--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ hostnamectl set-hostname "$HOSTNAME"
 
 # ---
 
+echo "###############################################"
+echo '# Installing required packages for XFCE desktop...'
+echo "###############################################"
+
 sudo dnf install xorg-x11-server-Xorg xorg-x11-xinit network-manager-applet\
  xorg-x11-drv-libinput mesa-dri-drivers xfce4-panel xfce4-datetime-plugin\
  xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop\
@@ -19,13 +23,18 @@ sudo dnf install xorg-x11-server-Xorg xorg-x11-xinit network-manager-applet\
  gvfs lightdm-gtk xfwm4 NetworkManager-wifi; # Required package for XFCE desktop
 
 # ---
+echo "###############################################"
+echo '# Enabling lightdm display manager...'
+echo "###############################################"
 
 systemctl enable lightdm; # Enable login using graphical interface
 systemctl set-default graphical.target; # Boot to graphical interface as default
 
 # ---
 
-echo 'Install RPM Fusion...'
+echo "###############################################"
+echo '# Installing RPM Fusion & Flathub...'
+echo "###############################################"
 
 sudo dnf install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
@@ -46,9 +55,16 @@ echo 'Upgrade DNF...'
 
 sudo dnf upgrade --refresh -qy
 
+
+echo "###############################################"
+echo '# RPM Fusion & Flathub Installed...'
+echo "###############################################"
+
 # ---
 
-echo 'Install DNF packages...'
+echo "###############################################"
+echo '# Installing DNF packages...'
+echo "###############################################"
 
 sudo dnf install -qy \
     firefox \
@@ -59,15 +75,120 @@ sudo dnf install -qy \
     htop \
     neofetch \
     xfce4-whiskermenu-plugin \
+    light-locker \
+    xfce4-screenshooter \
     viewnior \
     abiword \
     flameshot \
-    gcolor3 
+    wget \
+    xfce4-docklike-plugin \
+    tar\
+    rofi \
+    papirus-icon-theme.noarch \
+    google-noto-cjk-fonts-common.noarch \
+    gcolor2 
+
+
+echo "###############################################"
+echo '# DNF packages installed...'
+echo "###############################################"
+
 # ---
 
-echo 'Finished...'
+echo "###############################################"
+echo "# Creating private folders for later use"
+echo "###############################################"
+
+set -e
+
+[ -d $HOME"/.icons" ] || mkdir -p $HOME"/.icons"
+[ -d $HOME"/Documents" ] || mkdir -p $HOME"/Documents"
+[ -d $HOME"/Pictures/wallpapers" ] || mkdir -p $HOME"/Pictures/wallpapers"
+[ -d $HOME"/.themes" ] || mkdir -p $HOME"/.themes"
+[ -d $HOME"/.local/share/fonts" ] || mkdir -p $HOME"/.local/share/fonts"
+[ -d $HOME"/.config/gtk-3.0" ] || mkdir -p $HOME"/.config/gtk-3.0"
+[ -d $HOME"/.config/kitty" ] || mkdir -p $HOME"/.config/kitty"
+[ -d $HOME"/.config/geany" ] || mkdir -p $HOME"/.config/geany"
+[ -d $HOME"/.config/rofi" ] || mkdir -p $HOME"/.config/rofi"
+[ -d $HOME"/.config/xfce4/terminal" ] || mkdir -p $HOME"/.config/xfce4/terminal"
 
 
+echo "###############################################"
+echo "# Personal folders created or existed already"
+echo "###############################################"
+
+# ---
+
+echo "###############################################"
+echo "# Copy fonts to .local/share/fonts/"
+echo "###############################################"
+
+cp -R ~/fedora/fonts/* ~/.local/share/fonts/
+
+fc-cache -fv ~/.fonts
+
+echo "############################################"
+echo "# Fonts have been copied and refreshed       "
+echo "############################################"
+
+# ---
+
+echo "############################################"
+echo "# Copy themes & icons to .themes & .icons"
+echo "############################################"
+
+cp -R ~/fedora/themes/* ~/.themes/
+
+fc-cache -fv ~/.themes
+
+# ---
+
+cp -R ~/fedora/icons/* ~/.icons/
+
+fc-cache -fv ~/.icons
+
+
+echo "############################################"
+echo "# Themes have been copied and refreshed   "
+echo "############################################"
+
+# ---
+
+echo "############################################"
+echo "# Copying files/folders i.e. gtk.css, kitty, geany, rofi, terminalrc"
+echo "############################################"
+
+cp -R ~/fedora/gtk.css ~/.config/gtk-3.0/
+cp -R ~/fedora/kitty/* ~/.config/kitty/
+cp -R ~/fedora/rofi/* ~/.config/rofi/
+cp -R -f ~/fedora/terminalrc ~/.config/xfce4/terminal/
+cp -R -f ~/fedora/geany/* ~/.config/geany/
+
+echo "############################################"
+echo "# Files/folders copied"
+echo "############################################"
+
+# ---
+
+echo "############################################"
+echo "# Copying wallpaperss"
+echo "############################################"
+
+
+cp -R ~/fedora/wallpapers/* ~/Pictures/wallpapers/
+
+
+echo "############################################"
+echo "# Wallpapers copied"
+echo "############################################"
+
+# ---
+
+echo "############################################"
+echo "############################################"
+echo "FINISHED..."
+echo "############################################"
+echo "############################################"
 
 
 
